b50e367772eb3e3f89b4eebef8428b51f62c6bb4
arhamfarman <arham.farman@gmail.com>
2021-06-07 12:41:26 +0500

    Commit

---
+++ app/src/main/java/com/example/webview/MainActivity.java
@@ -0,0 +1,25 @@
+package com.example.webview;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends AppCompatActivity {
+
+    WebView webView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        webView = findViewById(R.id.webview);
+
+        webView.loadUrl("http://google.com");
+        webView.setWebViewClient(new WebViewClient());
+
+
+    }
+}
